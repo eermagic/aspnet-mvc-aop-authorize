73db8566c40d62ac33c76d7da7cf192217cd5b27
Mars <eermagic@yahoo.com.tw>
2022-06-29 17:16:02 +0800

    首次上傳

---
+++ AuthorizeCheck/Controllers/HomeController.cs
@@ -0,0 +1,70 @@
+﻿using AuthorizeCheck.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthorizeCheck.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+
+        /// <summary>
+        /// 擁有會員權限才可訪問
+        /// </summary>
+        /// <returns></returns>
+        [AuthorizeFilter(UserRole.Member)]
+        public ActionResult MemberPage()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 擁有管理員權限才可訪問
+        /// </summary>
+        /// <returns></returns>
+        [AuthorizeFilter(UserRole.Admin)]
+        public ActionResult AdminPage()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 測試登入
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult LoginTest()
+        {
+            // 省略資料庫查詢動作...
+
+            // 測試會員登入身份
+            Session["UserRole"] = "Member";
+            return View();
+        }
+    }
+}
---
+++ AuthorizeCheck/Controllers/MemberController.cs
@@ -0,0 +1,19 @@
+﻿using AuthorizeCheck.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AuthorizeCheck.Controllers
+{
+    [AuthorizeFilter(UserRole.Member)]
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
